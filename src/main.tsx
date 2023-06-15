7ddb7f1e39d23df44484694290d04eed52b95f67
Rapha-btc <rapha@fontainebleau-management.com>
2023-06-15 18:21:30 +0000

    Hello world - bug fixed with Friedger s help as well as Hugo and Brice! thank you! :P

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
+import { Connect } from "@stacks/connect-react";
+import { userSession } from "./components/ConnectWallet";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <Connect
+      authOptions={{
+        appDetails: {
+          name: "Stacks React Template",
+          // todo:
+          icon: window.location.origin + "/logo.png",
+        },
+        redirectTo: "/",
+        onFinish: () => {
+          window.location.reload();
+        },
+        userSession,
+      }}
+    >
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </Connect>
   </React.StrictMode>
 );
