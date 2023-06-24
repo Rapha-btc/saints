bbc19c91b5e9959123047ff79750c4121d5b965d
Rapha-btc <rapha@fontainebleau-management.com>
2023-06-24 03:01:46 +0000

    Center

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ import CollatSlider from "./components/CollatSlider";
 
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { Center } from "@chakra-ui/react";
 
 interface Status {
   chain_tip: {
@@ -106,137 +107,144 @@ function App() {
     userAddress = userSession.loadUserData().profile.stxAddress.testnet;
   }
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
+    <Center>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav" "main"`, // `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        {/* <Show above="lg">
         <GridItem area="aside"></GridItem>
-      </Show>
-      <GridItem area="main">
-        <Card maxW="sm" align="center">
-          <CardBody>
-            {/* <Image
+      </Show> */}
+        <GridItem area="main">
+          <Card maxW="sm" align="center">
+            <CardBody>
+              {/* <Image
           src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
           alt="Green double couch with wooden legs"
           borderRadius="lg"
         /> */}
-            <Stack mt="6" spacing="3">
-              <Heading colorScheme="purple" size="xl" textAlign="center">
-                sBTC call options
-              </Heading>
-              <Text fontSize="xs" as="em">
-                When creating a CALL, you deposit 3m sats and set a strike price
-                in STX. A CALL holder can then buy these 3m sats at your set
-                price by sending you the strike price. Their right to do so
-                expires after a predetermined block number.
-              </Text>
-              <ExpirationSelector
-                selectedExpiration={callQuery.expiration}
-                onSelectExpiration={(expiration) => {
-                  if (data && data.chain_tip) {
-                    const call_expire_at = getStandardExpiration(
-                      data.chain_tip.block_height,
-                      expiration.name
-                    );
+              <Stack mt="6" spacing="3">
+                <Heading colorScheme="purple" size="xl" textAlign="center">
+                  sBTC call options
+                </Heading>
+                <Text fontSize="xs" as="em">
+                  When creating a CALL, you deposit 3m sats and set a strike
+                  price in STX. A CALL holder can then buy these 3m sats at your
+                  set price by sending you the strike price. Their right to do
+                  so expires after a predetermined block number.
+                </Text>
+                <ExpirationSelector
+                  selectedExpiration={callQuery.expiration}
+                  onSelectExpiration={(expiration) => {
+                    if (data && data.chain_tip) {
+                      const call_expire_at = getStandardExpiration(
+                        data.chain_tip.block_height,
+                        expiration.name
+                      );
+                      setCallQuery({
+                        ...callQuery,
+                        expiration: expiration,
+                        call_expire_at: uintCV(call_expire_at),
+                      });
+                    } else {
+                      console.log("no data");
+                    }
+                  }}
+                />
+                {callQuery.expiration && (
+                  <ExpirationBlock
+                    expiration_name={callQuery.expiration.name}
+                  />
+                )}
+
+                <Text color="blue.600" fontSize="xl">
+                  Strike-price in stx
+                </Text>
+                <StrikeInput
+                  onStrikeChange={(
+                    valueAsString: string,
+                    valueAsNumber: number
+                  ) =>
                     setCallQuery({
                       ...callQuery,
-                      expiration: expiration,
-                      call_expire_at: uintCV(call_expire_at),
-                    });
-                  } else {
-                    console.log("no data");
+                      strike: uintCV(valueAsNumber),
+                    })
                   }
-                }}
-              />
-              {callQuery.expiration && (
-                <ExpirationBlock expiration_name={callQuery.expiration.name} />
-              )}
-
-              <Text color="blue.600" fontSize="xl">
-                Strike-price in stx
-              </Text>
-              <StrikeInput
-                onStrikeChange={(
-                  valueAsString: string,
-                  valueAsNumber: number
-                ) =>
-                  setCallQuery({
-                    ...callQuery,
-                    strike: uintCV(valueAsNumber),
-                  })
-                }
-              />
+                />
+                <Text fontSize="xs" as="em">
+                  Strike-price applies to all Calls.
+                </Text>
+                <Text color="orange.600" fontSize="xl">
+                  sBTC-locked in sats
+                </Text>
+                <CollatSlider
+                  onCollateralChange={(valueAsNumber: number) =>
+                    setCallQuery({
+                      ...callQuery,
+                      btclocked: uintCV(valueAsNumber * 1000000),
+                    })
+                  }
+                />
+                <Text fontSize="xs" as="em">
+                  Each increment of 3m sats represents a Call.
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="6">
+                <ContractCallVote
+                  contractAddress={cAdd}
+                  contractName="sbtc"
+                  functionName="mint"
+                  functionArgs={[
+                    uint(1000000),
+                    standardPrincipalCV(userAddress),
+                  ]}
+                  postConditions={[]}
+                  buttonLabel="Free sBTC 🍊"
+                  buttoncolor="blue"
+                />
+                <ContractCallVote
+                  contractAddress={cAdd}
+                  contractName="bitcoin-call"
+                  functionName="mint"
+                  functionArgs={[
+                    cpCV,
+                    callQuery.call_expire_at, // uint(2100), //
+                    callQuery.btclocked,
+                    callQuery.strike,
+                  ]} // callQuery.strike // expiration: callQuery.expiration
+                  postConditions={[]}
+                  buttonLabel="Mint Calls 🍎"
+                  buttoncolor="orange"
+                />
+              </ButtonGroup>
+            </CardFooter>
+            <CardBody>
               <Text fontSize="xs" as="em">
-                Strike-price applies to all Calls.
+                If your CALLS aren't meeting your expectations, you do have the
+                option to cancel them, which allows you to reclaim your locked
+                sBTC. Just keep in mind that this applies only as long as your
+                CALLS are still in your possession. Once your CALLS have been
+                transferred or sold, and thus are no longer owned by you, the
+                option to cancel them is no longer available.
               </Text>
-              <Text color="orange.600" fontSize="xl">
-                sBTC-locked in sats
-              </Text>
-              <CollatSlider
-                onCollateralChange={(valueAsNumber: number) =>
-                  setCallQuery({
-                    ...callQuery,
-                    btclocked: uintCV(valueAsNumber * 1000000),
-                  })
-                }
-              />
+            </CardBody>
+            <CardBody>
               <Text fontSize="xs" as="em">
-                Each increment of 3m sats represents a Call.
+                Testnet sBTC has no value.
               </Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <ButtonGroup spacing="6">
-              <ContractCallVote
-                contractAddress={cAdd}
-                contractName="sbtc"
-                functionName="mint"
-                functionArgs={[uint(1000000), standardPrincipalCV(userAddress)]}
-                postConditions={[]}
-                buttonLabel="Free sBTC 🍊"
-                buttoncolor="blue"
-              />
-              <ContractCallVote
-                contractAddress={cAdd}
-                contractName="bitcoin-call"
-                functionName="mint"
-                functionArgs={[
-                  cpCV,
-                  callQuery.call_expire_at, // uint(2100), //
-                  callQuery.btclocked,
-                  callQuery.strike,
-                ]} // callQuery.strike // expiration: callQuery.expiration
-                postConditions={[]}
-                buttonLabel="Mint Calls 🍎"
-                buttoncolor="orange"
-              />
-            </ButtonGroup>
-          </CardFooter>
-          <CardBody>
-            <Text fontSize="xs" as="em">
-              If your CALLS aren't meeting your expectations, you do have the
-              option to cancel them, which allows you to reclaim your locked
-              sBTC. Just keep in mind that this applies only as long as your
-              CALLS are still in your possession. Once your CALLS have been
-              transferred or sold, and thus are no longer owned by you, the
-              option to cancel them is no longer available.
-            </Text>
-          </CardBody>
-          <CardBody>
-            <Text fontSize="xs" as="em">
-              Testnet sBTC has no value.
-            </Text>
-          </CardBody>
-        </Card>
-      </GridItem>
-    </Grid>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </Grid>
+    </Center>
   );
   // return ;
 }
